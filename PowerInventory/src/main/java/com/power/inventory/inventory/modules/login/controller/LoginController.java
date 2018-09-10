b66ed92cba8d6482e71567906e17d3e85b72b543
liangsm <foodming@hotmail.com>
2018-09-10 19:38:10 +0800

    git for test

--- PowerInventory/src/main/java/com/power/inventory/inventory/modules/login/controller/LoginController.java
+++ PowerInventory/src/main/java/com/power/inventory/inventory/modules/login/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//test for git
 /**
  * 进入登陆页面
  */
